e3ecc644e2b3cb3ce03c626748b5334fc31ab480
Your Name <you@example.com>
2022-05-27 19:18:38 +0000

    mut, begin

--- select_mut.sh
+++ select_mut.sh
@@ -1,5 +1,27 @@
 
-output=$(mysql genetic --user=genetic --password=Str1ktG3h31^^ -se "SELECT path FROM genetic ORDER BY score DESC limit 100;")
+output=$(mysql genetic --user=genetic --password=Str1ktG3h31^^ -se "SELECT path FROM genetic ORDER BY score DESC limit 1000;")
 
-echo "$output"
+counter=0
+rand=$(($RANDOM/2000))
 
+line_counter=0
+
+while read p; do
+
+        res=$((($counter + $rand) %100))
+        
+        if ! (($res)) 
+        then 
+        	echo $p
+        	rand=$(($RANDOM/2000))
+        	line_counter=$((line_counter+1))
+        fi
+        
+        if [[ line_counter -eq 10 ]]
+        then
+        	break
+        fi
+        
+  counter=$((counter+1))
+
+done <<< "$output"
