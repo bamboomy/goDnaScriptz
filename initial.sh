14ac4a10fc40e5ff5539455a898856b5b7c6aaa7
Your Name <you@example.com>
2022-06-04 18:41:55 +0000

    snap

---
+++ initial.sh
@@ -0,0 +1,4 @@
+for i in dna{1..1000}
+do
+   java -jar writer.jar $i
+done
--- parseNewGen.sh
+++ parseNewGen.sh
@@ -8,14 +8,14 @@ stop=false
 for i in ../gens/1/children/child*
 do
 
+	echo $i 0 $counter
+
 	if ! ./two.sh $i 0 $counter; then
 	
 		stop=true
 		
 		break
 	fi
-	
-	echo $i 0 $counter
 	
 	counter=$((counter+1))
 done
@@ -25,14 +25,14 @@ if [ "$stop" = "false" ]; then
 	for i in ../gens/1/mutated/*
 	do
 
+	        echo $i 0 $counter
+
 		if ! ./two.sh $i 0 $counter; then
 
                 	stop=true
 
 	                break
         	fi
-
-	        echo $i 0 $counter
 
         	counter=$((counter+1))
 	done
